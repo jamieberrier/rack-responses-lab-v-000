7ec1b2eff7ae1ff50f04b43d201427730dba1302
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 19:33:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    if timestamp <= 12
+    if timestamp < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
